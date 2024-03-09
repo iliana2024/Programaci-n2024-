9cc532e8ee0864bca2a201336c56135ce30fe68b
iliana2024 <160266131+iliana2024@users.noreply.github.com>
2024-03-09 11:03:22 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParcial_Parte1
+{
+    internal class Calculadora
+    {
+        public static int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Restar(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
