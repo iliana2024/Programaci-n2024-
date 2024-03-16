e1a4ef8de1cfad294748308beb68fc8a032b55c8
iliana2024 <160266131+iliana2024@users.noreply.github.com>
2024-03-15 21:12:49 -0600

    Add files via upload

---
+++ tareaDesafios6/parte2.cs
@@ -0,0 +1,35 @@
+﻿public class superficieFrontal
+{
+    private double alto;
+    private double largo;
+
+    // Constructor
+    public superficieFrontal(double alto, double largo)
+    {
+        this.alto = alto;
+        this.largo = largo;
+    }
+
+    // Método para calcular la superficie frontal
+    public double calcularSuperficieFrontal()
+    {
+        return alto * largo;
+    }
+
+    // Getter para el miembro variable SuperficieFrontal
+    public double getSuperficieFrontal()
+    {
+        return calcularSuperficieFrontal();
+    }
+
+    // Getters para alto y largo (opcional)
+    public double getAlto()
+    {
+        return alto;
+    }
+
+    public double getLargo()
+    {
+        return largo;
+    }
+}
---
+++ tareaDesafios6/program.cs.cs
@@ -0,0 +1,34 @@
+﻿
+ class SuperficieFrontal 
+{
+    private double alto;
+    private double largo;
+
+    public SuperficieFrontal(double alto, double largo)
+    {
+        this.alto = alto;
+        this.largo = largo;
+    }
+
+    public double calcularSuperficieFrontal()
+    {
+        return alto * largo;
+    }
+
+   
+    public double getSuperficieFrontal()
+    {
+        return calcularSuperficieFrontal();
+    }
+
+ 
+    public double getAlto()
+    {
+        return alto;
+    }
+
+    public double getLargo()
+    {
+        return largo;
+    }
+}
